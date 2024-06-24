0a4136a206c0ed280d4cd26752ad77cdf0609a1d
yaroslav.GOMZ.R.Y.A.W <gomzinaroslav938@gmail.com>
2024-06-24 21:11:18 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf_3.src;
+
+namespace Wpf_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<ToDo> toDoList = new List<ToDo>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DescriptionToDo.Text = "Описания нет";
+            DateToDo.SelectedDate = DateTime.Today.AddDays(1);
+
+            toDoList.Add(new ToDo("Родиться", new DateTime(2024,01,10), "Важно!"));
+            toDoList.Add(new ToDo("Посадить сына", new DateTime(2024, 01, 11), "Важно!!"));
+            toDoList.Add(new ToDo("Построить дерево", new DateTime(2024, 01, 12), "Важно!!!"));
+            toDoList.Add(new ToDo("Вырастить дом", new DateTime(2024, 01, 13), "Важно!!!!"));
+            toDoList.Add(new ToDo("умереть", new DateTime(2024, 01, 14), "Важно!!!!!"));
+            RefreshToDoList();
+            CheckboxEnableToDo_Unchecked(Owner, new RoutedEventArgs());
+
+
+        }
+
+        private void RefreshToDoList()
+        {
+            ListToDo.ItemsSource = null;
+            ListToDo.ItemsSource = toDoList;
+        }
+        private void CheckboxEnableToDo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if(GroupBoxToDo == null || ButtonAdd == null) return;
+            GroupBoxToDo.Visibility = Visibility.Hidden;
+            ButtonAdd.Visibility = Visibility.Hidden;
+        }
+        private void CheckboxEnableToDo_Checked(object sender, RoutedEventArgs e)
+        {
+            if (GroupBoxToDo == null || ButtonAdd == null) return;
+            GroupBoxToDo.Visibility = Visibility.Visible;
+            ButtonAdd.Visibility = Visibility.Visible;
+        }
+        private void ButtonRemoveToDo_Click(object sender, RoutedEventArgs e)
+        {
+            toDoList.Remove(ListToDo.SelectedItem as ToDo);
+            RefreshToDoList();
+        }
+
+        private void ButtonAddToDo_Click(object sender, RoutedEventArgs e)
+        {
+            if(!DateToDo.SelectedDate.HasValue)
+            {
+                MessageBox.Show("Дата повесилась", Name = "Ошибка");
+                return;
+            }
+            toDoList.Add(new ToDo(TitleToDo.Text,
+                (DateTime)DateToDo.SelectedDate,
+                DescriptionToDo.Text));
+            TitleToDo.Text = null;
+            DescriptionToDo.Text = "Описания нет";
+            RefreshToDoList();
+        }
+    }
+}
