ccc21ac2630831e92e6ccb1517afa2b0c0603b3a
yaroslav.GOMZ.R.Y.A.W <gomzinaroslav938@gmail.com>
2024-06-26 21:17:49 +0300

    11

--- CreateToDO.xaml.cs
+++ CreateToDO.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.ComponentModel;
-using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,45 +10,39 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Wpf_3.src;
 
 namespace Wpf_3
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для CreateToDO.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CreateToDO : Window
     {
-        public List<ToDo> toDoList = new List<ToDo>();
-        public MainWindow()
+        
+        public CreateToDO()
         {
             InitializeComponent();
+            //this.Owner.Title = "asdda"; - выдаст исключение
             DescriptionToDo.Text = "Описания нет";
             DateToDo.SelectedDate = DateTime.Today.AddDays(1);
 
-            toDoList.Add(new ToDo("Родиться", new DateTime(2024,01,10), "Важно!"));
+            toDoList.Add(new ToDo("Родиться", new DateTime(2024, 01, 10), "Важно!"));
             toDoList.Add(new ToDo("Посадить сына", new DateTime(2024, 01, 11), "Важно!!"));
             toDoList.Add(new ToDo("Построить дерево", new DateTime(2024, 01, 12), "Важно!!!"));
             toDoList.Add(new ToDo("Вырастить дом", new DateTime(2024, 01, 13), "Важно!!!!"));
             toDoList.Add(new ToDo("умереть", new DateTime(2024, 01, 14), "Важно!!!!!"));
             RefreshToDoList();
             CheckboxEnableToDo_Unchecked(Owner, new RoutedEventArgs());
-
-        }
-
-        private void RefreshToDoList()
-        {
-            ListToDo.ItemsSource = null;
-            ListToDo.ItemsSource = toDoList;
         }
         private void CheckboxEnableToDo_Unchecked(object sender, RoutedEventArgs e)
         {
-            if(GroupBoxToDo == null || ButtonAdd == null) return;
+            if (GroupBoxToDo == null || ButtonAdd == null) return;
             GroupBoxToDo.Visibility = Visibility.Hidden;
             ButtonAdd.Visibility = Visibility.Hidden;
         }
+
         private void CheckboxEnableToDo_Checked(object sender, RoutedEventArgs e)
         {
             if (GroupBoxToDo == null || ButtonAdd == null) return;
@@ -57,15 +53,14 @@ namespace Wpf_3
             createToDO.Owner = this;
             //this.Close();
         }
-        private void ButtonRemoveToDo_Click(object sender, RoutedEventArgs e)
+        private void RefreshToDoList()
         {
-            toDoList.Remove(ListToDo.SelectedItem as ToDo);
-            RefreshToDoList();
+            ListToDo.ItemsSource = null;
+            ListToDo.ItemsSource = toDoList;
         }
-
         private void ButtonAddToDo_Click(object sender, RoutedEventArgs e)
         {
-            if(!DateToDo.SelectedDate.HasValue)
+            if (!DateToDo.SelectedDate.HasValue)
             {
                 MessageBox.Show("Дата повесилась", Name = "Ошибка");
                 return;
@@ -75,6 +70,11 @@ namespace Wpf_3
                 DescriptionToDo.Text));
             TitleToDo.Text = null;
             DescriptionToDo.Text = "Описания нет";
+            RefreshToDoList();
+        }
+        private void ButtonRemoveToDo_Click(object sender, RoutedEventArgs e)
+        {
+            toDoList.Remove(ListToDo.SelectedItem as ToDo);
             RefreshToDoList();
         }
     }
